4851b3f0ebb8ee49a7e0c63e89703d648c0663cd
Peter Herdenborg <peter.herdenborg@biolit.se>
2024-12-20 09:42:10 +0100

    Replaced hardcoded details data with call to OMDB API

--- movies/views.py
+++ movies/views.py
@@ -2,6 +2,8 @@ import re
 import json
 import requests
 
+API_KEY = '444ddd63'
+
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
 
@@ -12,55 +14,11 @@ def index(request):
 
 def details(request, imdbID):
     template = loader.get_template("movies/details.html")
-    db = [{
-        "Title": "Blade Runner",
-        "Year": "1982",
-        "Rated": "R",
-        "Released": "25 Jun 1982",
-        "Runtime": "117 min",
-        "Genre": "Sci-Fi, Thriller",
-        "Director": "Ridley Scott",
-        "Writer": "Hampton Fancher (screenplay), David Webb Peoples (screenplay), Philip K. Dick (novel)",
-        "Actors": "Harrison Ford, Rutger Hauer, Sean Young, Edward James Olmos",
-        "Plot": "A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.",
-        "Language": "English, German, Cantonese, Japanese, Hungarian, Arabic",
-        "Country": "USA",
-        "Awards": "Nominated for 2 Oscars. Another 11 wins & 16 nominations.",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BNzQzMzJhZTEtOWM4NS00MTdhLTg0YjgtMjM4MDRkZjUwZDBlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-        "Ratings": [
-            {
-            "Source": "Internet Movie Database",
-            "Value": "8.2/10"
-            },
-            {
-            "Source": "Rotten Tomatoes",
-            "Value": "90%"
-            },
-            {
-            "Source": "Metacritic",
-            "Value": "89/100"
-            }
-        ],
-        "Metascore": "89",
-        "imdbRating": "8.2",
-        "imdbVotes": "633,160",
-        "imdbID": "tt0083658",
-        "Type": "movie",
-        "DVD": "27 Aug 1997",
-        "BoxOffice": "N/A",
-        "Production": "Warner Bros. Pictures",
-        "Website": "https://www.warnerbros.com/blade-runner",
-        "Response": "True"
-    }]
-
-    matches = list(filter(lambda movie: movie['imdbID'] == imdbID, db))
-    print("matches")
-    print(matches[0])
-
-    return HttpResponse(template.render({ "details": matches[0] }, request))
+    response = requests.get(f'http://www.omdbapi.com/?apikey={API_KEY}&i={imdbID}') 
+    match= response.json()
+    return HttpResponse(template.render({ "details": match }, request))
 
 def search(request, query):
-    regex = re.compile(query, re.IGNORECASE)
     response = requests.get('http://www.omdbapi.com/?apikey=444ddd63&s=' + query) 
     matches = response.json()['Search']
     return JsonResponse(matches, safe=False)
